ad921d0019eb46704c648befb487e49c731cc81b
GokdenizCakir <gokdeniz.cakir@std.yildiz.edu.tr>
2023-01-27 16:55:08 +0300

    add team logos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import LandingPage from './pages/LandingPage';
-import Teams from './pages/Teams';
-import Nav from './pages/Nav';
+import LandingPage from "./pages/LandingPage";
+import Teams from "./pages/Teams";
+import Nav from "./pages/Nav";
 
 function App() {
-	return (
-		<div className='App'>
-			<div className='font-bebasNeue bg-customDarkPurple flex flex-row justify-center text-customLightPink tracking-[0.16em]'>
-				<Nav />
-				<div className='flex flex-col justify-center w-full'>
-					<LandingPage />
-					<Teams />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="font-bebasNeue bg-customDarkPurple flex flex-row justify-center text-customLightPink tracking-[0.16em]">
+        <Nav />
+        <div className="flex flex-col justify-center w-full">
+          <LandingPage />
+          <Teams />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import teams from "./../data/teams.json";
+// import logo from "../assets/SkylabPinkLogo.png";
 
 const Teams = () => {
   const [teamSelect, setTeamSelect] = useState(true);
+  const [argeSelected, setArgeSelected] = useState(0);
 
   return (
     <section className="h-screen pt-36 bg-customDarkPurple white">
-      <div className=" text-customLightPink text-2xl my-12 flex justify-center">
+      <div className=" text-customLightPink text-2xl my-16 flex justify-center">
         <div className="flex justify-between w-auto items-center">
           <div
             onClick={() => setTeamSelect(true)}
@@ -26,15 +28,28 @@ const Teams = () => {
           </div>
         </div>
       </div>
-      <div className="w-auto mx-36">
-        <div>
-          {teams.map((team, index) => {
-            return(
-              <div></div>
-            )
+      <div>
+        <div className="flex justify-center space-x-6">
+          {(teamSelect ? teams.arge : teams.social).map((team, index) => {
+            return (
+              <>
+                <div
+                  className={`w-[5.2rem] h-[5.2rem] flex justify-center items-center outline-customAccent ${
+                    argeSelected === index ? "outline-8 outline" : null
+                  } p-2 cursor-pointer bg-customLightPink overflow-hidden rounded-[50%]`}
+                >
+                  <img
+                    onClick={() => setArgeSelected(index)}
+                    src={require("../assets/" + team.logo)}
+                    className="object-cover"
+                    key={index}
+                  />
+                </div>
+                <div className="text-white">{teams.name}</div>
+              </>
+            );
           })}
         </div>
-        <div></div>
       </div>
     </section>
   );
