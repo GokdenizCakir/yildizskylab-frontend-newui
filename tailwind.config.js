9f0fd94610d54e7c29f5116da7ac1a29bb8fe700
Furkan Kartal <furkankartalus@gmail.com>
2023-01-27 16:49:19 +0300

    add image to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        customDarkPurple: "#06032C",
-        customLightPink: "#EADAFF",
-        customLightPurple: "#7C77B3",
-        customAccent: "#27A68E",
-      },
-      fontFamily: {
-        bebasNeue: ["Bebas Neue"],
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'landing-page': "url('/src/assets/landing-page.png')",
+			},
+			colors: {
+				customDarkPurple: '#06032C',
+				customLightPink: '#EADAFF',
+				customLightPurple: '#7C77B3',
+				customAccent: '#27A68E',
+			},
+			fontFamily: {
+				bebasNeue: ['Bebas Neue'],
+			},
+		},
+	},
+	plugins: [],
 };
