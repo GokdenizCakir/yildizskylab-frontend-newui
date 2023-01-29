404e3f41690bbe9885c59dded0fa9820546d9118
Furkan Kartal <furkankartalus@gmail.com>
2023-01-29 19:38:17 +0300

    landing page mostly finished

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const LandingPage = () => {
 	return (
-		<div className='h-screen  pt-36 bg-landing-page bg-cover bg-[0%] flex justify-center items-center'>
-			<div className='flex flex-row '>
-				<div className='w-[1000px] bg-customLightPink h-[400px]'></div>
-				<div className='flex flex-col items-center justify-center -mb-28 space-y-5 w-[calc(100vw-5vw)]'>
+		<div className='h-screen pt-36 bg-landing-page bg-cover bg-[0%] flex justify-center items-center'>
+			<div className='flex flex-row overflow-x-scroll overflow-clip px-12'>
+				<div className='flex-shrink-0 w-[700px] bg-customLightPink h-[400px] '></div>
+				<div className='flex flex-col items-center justify-center -mb-28 space-y-5 w-[calc(100vw-5vw)] translate-x-0.5 flex-shrink-0'>
 					<span className='text-[4rem] leading-none'>
 						YILDIZ TEKNİK ÜNİVERSİTESİ
 					</span>
@@ -13,7 +13,7 @@ const LandingPage = () => {
 						SKY LAB
 					</span>
 				</div>
-				<div className='w-[1000px] bg-customLightPink h-[400px]'></div>
+				<div className='flex-shrink-0 w-[700px] bg-customLightPink h-[400px]'></div>
 			</div>
 			<div className=''></div>
 		</div>
