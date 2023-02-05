5a5a99d239c2f18d84d34f6e30fda2b71d2ce219
Furkan Kartal <furkankartalus@gmail.com>
2023-02-06 02:04:13 +0300

    Fix page load before loading animation finishes

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,42 +1,48 @@
-import { useEffect, useState } from 'react';
-import logo from './../assets/SkylabPinkLogo.png';
+import { useEffect, useState, lazy, Suspense } from 'react';
+// const Logo = lazy(() => import('./../assets/SkylabPinkLogo.png'));
+import Logo from './../assets/SkylabPinkLogo.png';
 
 const Loading = ({ loaded }) => {
-  const [progress, setProgress] = useState('w-[0%]');
-  const [visible, setVisible] = useState(true);
+	const [progress, setProgress] = useState('w-[0%]');
+	const [visible, setVisible] = useState(true);
+	const [hidden, setHidden] = useState('');
 
-  useEffect(() => {
-    setProgress('w-[90%]');
-  }, []);
+	useEffect(() => {
+		setProgress('w-[90%]');
+	}, []);
 
-  const handleLoad = () => {
-    if (progress === 'w-[100%]') {
-      setVisible(false);
-      clearInterval(checkIfLoaded);
-    }
-    if (loaded) {
-      setProgress('w-[100%]');
-    }
-  };
+	const handleLoad = async () => {
+		if (progress === 'w-[100%]') {
+		}
+		if (loaded) {
+			setProgress('w-[100%]');
+			clearInterval(checkIfLoaded);
+			await new Promise(resolve => setTimeout(resolve, 600));
+			setVisible(false);
+			const myTimeout = setTimeout(() => setHidden('hidden'), 600);
+		}
+	};
 
-  const checkIfLoaded = setInterval(handleLoad, 1000);
+	const checkIfLoaded = setInterval(handleLoad, 1000);
 
-  return (
-    <section
-      className={`${
-        visible ? 'flex' : 'hidden'
-      } h-screen w-screen top-0 left-0 fixed justify-center items-center bg-customDarkPurple z-50`}
-    >
-      <div className='w-80 flex flex-col items-center'>
-        <img className='w-24' src={logo} alt='logo' />
-        <div className='w-full h-1 mt-6 bg-customLightPurple'>
-          <div
-            className={`${progress} h-full transition-all ease-out duration-[1800ms] bg-customLightPink`}
-          ></div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section
+			className={`${
+				visible ? 'opacity-100' : 'opacity-0'
+			} flex h-screen w-screen top-0 left-0 fixed justify-center items-center bg-customDarkPurple z-50 transition-all duration-700 ${hidden}`}
+		>
+			<div className='w-80 flex flex-col items-center'>
+				<Suspense fallback={<span>Loading</span>}>
+					<img className='w-24' src={Logo} alt='logo' />
+				</Suspense>
+				<div className='w-full h-1 mt-12 bg-customLightPurple'>
+					<div
+						className={` h-full transition-all ease-out duration-[1500ms] ${progress}  bg-customLightPink`}
+					></div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Loading;
