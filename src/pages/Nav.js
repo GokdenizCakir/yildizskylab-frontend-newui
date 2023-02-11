5a3f0a511beb117f664a8870fbbbc9c87181b493
Furkan Kartal <furkankartalus@gmail.com>
2023-02-11 23:32:44 +0300

    Adjust tracking for navbar links

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -4,42 +4,64 @@ import smoothscroll from 'smoothscroll-polyfill';
 smoothscroll.polyfill();
 
 const Nav = ({ refs: { landingRef, teamsRef, eventsRef, contactRef } }) => {
-  const handleScroll = ref => {
-    ref.current.scrollIntoView({ behavior: 'smooth' });
-  };
+	const handleScroll = ref => {
+		ref.current.scrollIntoView({ behavior: 'smooth' });
+	};
 
-  return (
-    <div className='h-36 w-full fixed top-0 flex flex-row items-center justify-between container z-50'>
-      <div className='flex items-center flex-row space-x-6'>
-        <div className='w-10 h-10'>
-          <img src={Logo} alt='' />
-        </div>
-        <div className='w-2 h-2 rounded-full bg-customLightPink'></div>
-        <div className=''>
-          <span className='font-bebasNeue text-5xl tracking-[0.3em] font-light'>
-            Sky Lab
-          </span>
-        </div>
-      </div>
-      <ul className='flex flex-row space-x-12 text-lg tracking-[0.16em]'>
-        <li>
-          <button onClick={() => handleScroll(landingRef)}>ANASAYFA</button>
-        </li>
-        <li>
-          <button onClick={() => handleScroll(teamsRef)}>EKİPLER</button>
-        </li>
-        <li>
-          <button onClick={() => handleScroll(eventsRef)}>ETKİNLİKLER</button>
-        </li>
-        <li>
-          <button onClick={() => handleScroll(contactRef)}>İLETİŞİM</button>
-        </li>
-        <li className='text-customAccent'>
-          <span>KAYIT OL/GİRİŞ YAP</span>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className='h-36 w-full fixed top-0 flex flex-row items-center justify-between container z-50'>
+			<div className='flex items-center flex-row space-x-6'>
+				<div className='w-10 h-10'>
+					<img src={Logo} alt='' />
+				</div>
+				<div className='w-2 h-2 rounded-full bg-customLightPink'></div>
+				<div className=''>
+					<span className='font-bebasNeue text-5xl tracking-[0.3em] font-light'>
+						Sky Lab
+					</span>
+				</div>
+			</div>
+			<ul className='flex flex-row space-x-12 text-lg '>
+				<li>
+					<button
+						className='tracking-[0.10em]'
+						onClick={() => handleScroll(landingRef)}
+					>
+						ANASAYFA
+					</button>
+				</li>
+				<li>
+					<button
+						className='tracking-[0.10em]'
+						onClick={() => handleScroll(teamsRef)}
+					>
+						EKİPLER
+					</button>
+				</li>
+				<li>
+					<button
+						className='tracking-[0.10em]'
+						onClick={() => handleScroll(eventsRef)}
+					>
+						ETKİNLİKLER
+					</button>
+				</li>
+				<li>
+					<button
+						className='tracking-[0.10em]'
+						onClick={() => handleScroll(contactRef)}
+					>
+						İLETİŞİM
+					</button>
+				</li>
+				<li className='text-customAccent'>
+					<a href='#' className='tracking-[0.10em]'>
+						KAYIT OL/GİRİŞ YAP
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Nav;
--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,103 +1,103 @@
 import React, {
-  forwardRef,
-  useImperativeHandle,
-  useRef,
-  useState,
+	forwardRef,
+	useImperativeHandle,
+	useRef,
+	useState,
 } from 'react';
 import teams from './../data/teams.json';
 import DownArrow from '../components/DownArrow';
 
 const Teams = forwardRef(function Teams(props, ref) {
-  const pageRef = useRef(null);
+	const pageRef = useRef(null);
 
-  useImperativeHandle(ref, () => {
-    return {
-      scrollIntoView() {
-        pageRef.current.scrollIntoView();
-      },
-    };
-  });
+	useImperativeHandle(ref, () => {
+		return {
+			scrollIntoView() {
+				pageRef.current.scrollIntoView();
+			},
+		};
+	});
 
-  const [teamSelect, setTeamSelect] = useState('arge');
-  const [teamIndex, setTeamIndex] = useState(0);
-  const [selectedTeam, setSelectedTeam] = useState('algolab');
+	const [teamSelect, setTeamSelect] = useState('arge');
+	const [teamIndex, setTeamIndex] = useState(0);
+	const [selectedTeam, setSelectedTeam] = useState('algolab');
 
-  return (
-    <section
-      className='h-screen snap-start relative pt-36 bg-customDarkPurple white'
-      ref={pageRef}
-    >
-      <div className=' text-customLightPink text-2xl my-16 flex justify-center'>
-        <div className='flex justify-between w-auto items-center'>
-          <div
-            onClick={() => {
-              setTeamSelect('arge');
-              setTeamIndex(0);
-              setSelectedTeam('algolab');
-            }}
-            className={`px-3 pt-[0.2rem] pb-[0.1rem] cursor-pointer leading-tight ${
-              teamSelect === 'arge' ? 'bg-customAccent' : 'bg-customDarkPurple'
-            } `}
-          >
-            ARGE EKİPLERİ
-          </div>
-          <div
-            onClick={() => {
-              setTeamSelect('social');
-              setTeamIndex(0);
-              setSelectedTeam('organizasyon');
-            }}
-            className={`px-3 pt-[0.2rem] py-[0.1rem] cursor-pointer ${
-              teamSelect === 'social'
-                ? 'bg-customAccent'
-                : 'bg-customDarkPurple'
-            } `}
-          >
-            SOSYAL EKİPLER
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className='flex justify-center space-x-12'>
-          {(teamSelect === 'arge' ? teams.arge : teams.social).map(
-            (team, index) => {
-              return (
-                <div
-                  key={index}
-                  className='flex flex-col justify-between items-center space-y-8'
-                >
-                  <div
-                    className={`w-[5.2rem] h-[5.2rem] flex justify-center items-center ring-customAccent ${
-                      teamIndex === index ? 'ring-8' : null
-                    } p-2 cursor-pointer bg-customLightPink overflow-hidden rounded-[50%]`}
-                  >
-                    <img
-                      onClick={() => {
-                        setTeamIndex(index);
-                        setSelectedTeam(team.name);
-                      }}
-                      src={require('../assets/' + team.logo)}
-                      className='object-cover w-[3.2rem] hue-rotate-[320deg] brightness-[0.2]'
-                      alt='logo'
-                    />
-                  </div>
-                  <div className='text-customLightPink text-lg text-end w-12 -rotate-45 '>
-                    {team.name}
-                  </div>
-                </div>
-              );
-            }
-          )}
-        </div>
-        <div className='w-screen flex mt-28 justify-center'>
-          <div className='font-inter w-1/2 text-center'>
-            {teams[teamSelect].find(team => team.name === selectedTeam).details}
-          </div>
-        </div>
-      </div>
-      <DownArrow />
-    </section>
-  );
+	return (
+		<section
+			className='h-screen snap-start relative pt-36 bg-customDarkPurple white'
+			ref={pageRef}
+		>
+			<div className=' text-customLightPink text-2xl my-16 flex justify-center'>
+				<div className='flex justify-between w-auto items-center'>
+					<div
+						onClick={() => {
+							setTeamSelect('arge');
+							setTeamIndex(0);
+							setSelectedTeam('algolab');
+						}}
+						className={`px-3 pt-[0.2rem] pb-[0.1rem] cursor-pointer leading-tight ${
+							teamSelect === 'arge' ? 'bg-customAccent' : 'bg-customDarkPurple'
+						} `}
+					>
+						ARGE EKİPLERİ
+					</div>
+					<div
+						onClick={() => {
+							setTeamSelect('social');
+							setTeamIndex(0);
+							setSelectedTeam('organizasyon');
+						}}
+						className={`px-3 pt-[0.2rem] py-[0.1rem] cursor-pointer ${
+							teamSelect === 'social'
+								? 'bg-customAccent'
+								: 'bg-customDarkPurple'
+						} `}
+					>
+						SOSYAL EKİPLER
+					</div>
+				</div>
+			</div>
+			<div>
+				<div className='flex justify-center space-x-12'>
+					{(teamSelect === 'arge' ? teams.arge : teams.social).map(
+						(team, index) => {
+							return (
+								<div
+									key={index}
+									className='flex flex-col justify-between items-center space-y-8'
+								>
+									<div
+										onClick={() => {
+											setTeamIndex(index);
+											setSelectedTeam(team.name);
+										}}
+										className={`w-[5.2rem] h-[5.2rem] flex justify-center items-center ring-customAccent ${
+											teamIndex === index ? 'ring-8' : null
+										} p-2 cursor-pointer bg-customLightPink overflow-hidden rounded-[50%]`}
+									>
+										<img
+											src={require('../assets/' + team.logo)}
+											className='object-cover w-[3.2rem] hue-rotate-[320deg] brightness-[0.2]'
+											alt='logo'
+										/>
+									</div>
+									<div className='text-customLightPink text-lg text-end w-12 -rotate-45 '>
+										{team.name}
+									</div>
+								</div>
+							);
+						}
+					)}
+				</div>
+				<div className='w-screen flex mt-28 justify-center'>
+					<div className='font-inter w-1/2 text-center'>
+						{teams[teamSelect].find(team => team.name === selectedTeam).details}
+					</div>
+				</div>
+			</div>
+			<DownArrow />
+		</section>
+	);
 });
 
 export default Teams;
