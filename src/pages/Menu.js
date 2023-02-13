e9d6ddf8f2311c2764e703bc71a72d51dea597be
Furkan Kartal <furkankartalus@gmail.com>
2023-02-13 22:37:19 +0300

    close menu on resize

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { menuState } from '../states/atom';
 
 const Menu = () => {
-  const menuOpened = useRecoilValue(menuState);
-  console.log(menuOpened);
-  return (
-    <div
-      className={`${
-        menuOpened ? null : '-translate-x-full'
-      } lg:hidden h-screen w-screen z-40 transition-all duration-300 bg-customLightPurple fixed`}
-    >
-      <div>Menu</div>
-    </div>
-  );
+	const menuOpened = useRecoilValue(menuState);
+	console.log(menuOpened);
+
+	return (
+		<div
+			className={`${
+				menuOpened ? null : '-translate-x-full'
+			} lg:hidden h-screen w-screen z-40 transition-all duration-300 bg-customLightPurple fixed`}
+		>
+			<div>Menu</div>
+		</div>
+	);
 };
 
 export default Menu;
