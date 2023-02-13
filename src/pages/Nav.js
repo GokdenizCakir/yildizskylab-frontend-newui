7871c37faae45ee8c86e17cb54e4de3fd44e876d
GokdenizCakir <gokdeniz.cakir@std.yildiz.edu.tr>
2023-02-13 19:07:07 +0300

    made navbar responsive

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -9,19 +9,22 @@ const Nav = ({ refs: { landingRef, teamsRef, eventsRef, contactRef } }) => {
 	};
 
 	return (
-		<div className='h-36 w-full fixed top-0 flex flex-row items-center justify-between container z-50'>
-			<div className='flex items-center flex-row space-x-6'>
-				<div className='w-10 h-10'>
+		<div className='h-24 lg:h-36 w-full fixed top-0 flex flex-row items-center justify-center lg:justify-between container z-50'>
+			<div className='ml-5 left-0 lg:hidden absolute'>
+				menu
+			</div>
+			<div className='flex items-center flex-row space-x-2 lg:space-x-6'>
+				<div className='w-9 h-9 lg:w-10 lg:h-10'>
 					<img src={Logo} alt='' />
 				</div>
-				<div className='w-2 h-2 rounded-full bg-customLightPink'></div>
+				<div className='hidden lg:flex w-2 h-2 rounded-full bg-customLightPink'></div>
 				<div className=''>
-					<span className='font-bebasNeue text-5xl tracking-[0.3em] font-light'>
+					<span className='font-bebasNeue text-3xl lg:text-5xl tracking-[0.3em] font-light'>
 						Sky Lab
 					</span>
 				</div>
 			</div>
-			<ul className='flex flex-row space-x-12 text-lg '>
+			<ul className='hidden lg:flex flex-row space-x-12 text-lg'>
 				<li>
 					<button
 						className='tracking-[0.10em]'
